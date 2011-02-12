3f48a29cf2595255722235ae7e2c4c41f3d7aca4
Chris Jensen <christophercjensen@gmail.com>
2011-02-12 07:08:14 -0800

    amended code license

--- PlexATV/Classes/PlexPreviewAsset.h
+++ PlexATV/Classes/PlexPreviewAsset.h
@@ -3,6 +3,9 @@
 //  atvTwo
 //
 //  Created by Frank Bauer on 27.10.10.
+//      Modified by ccjensen
+//
+//
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
 //  in the Software without restriction, including without limitation the rights
